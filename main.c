610424da60a275636c8ca029c7214fca9324cd68
Dakkay <teo.regge.99@gmail.com>
2021-08-11 03:09:15 +0200

    C project

--- main.c
+++ main.c
@@ -40,7 +40,9 @@ int main() {
     unsigned long graphCount=0;
     char streamS[30];
     char *stream=NULL;
-    fgets(streamS,(sizeof(char) * 30),stdin);
+    char *x;
+    x=fgets(streamS,(sizeof(char) * 30),stdin);
+    if(x);
     parser(streamS, value, parsedInt);
     //free(streamS);
     graphy list[parsedInt[1]];
@@ -60,6 +62,8 @@ int main() {
     //numbers = (unsigned long*) malloc(sizeof(unsigned long) * parsedInt[0]);
     unsigned long costs[parsedInt[0]];
     unsigned long nodeCache[parsedInt[0]];
+    int done=0;
+    unsigned long test[35];
 
     while (fgets(input, (sizeof(char) * 20), stdin) != NULL){
         //streamS = fgets(streamS,(sizeof(unsigned long) * 3*1),stdin);
@@ -92,8 +96,7 @@ int main() {
             //apply dijsktra algorithm
             unsigned long valueD;
             valueD=dijkstra(arr,parsedInt[0],costs,nodeCache);
-            //memorize first k graphs with their score
-            int done=0;
+            test[graphCount]=valueD;
             if(graphCount<parsedInt[1]){
                 list[graphCount].value=valueD;
                 list[graphCount].id=graphCount;
@@ -107,20 +110,21 @@ int main() {
                 }
                 if(valueD<list[0].value){
                     deleteMax(list,(int)parsedInt[1]);
-                    insert(list,(int)parsedInt[1],valueD,graphCount);
+                    insert(list,(int)parsedInt[1]-1,valueD,graphCount);
                 }
             }
             graphCount++;
         } else if (strncmp(input, "TopK\n",2)==0) {
             //free(streamS);
             int IV;
-            int checks,tree=0;
+            int checks;
             if(graphCount<parsedInt[1])checks = graphCount;
             else checks =parsedInt[1];
-            for(IV=0;IV<checks;IV++){
-                    if(list[IV].value!=-1)printf("%lu ",list[IV].id);
-
+            for(IV=0;IV<checks-1;IV++){
+                    printf("%lu ",list[IV].id);
+                    //printf("%lu ",list[IV].value);
             }
+            printf("%lu",list[IV].id);
             printf("\n");
             //IV++;
             //if(*(list+IV)!=-1)printf("%lu\n",*(list+IV));
@@ -245,8 +249,11 @@ unsigned long dijkstra(unsigned long *graph,int nodeNumber,unsigned long costs[]
     int I;
     unsigned long sum=0;
     unsigned long row=0;
-    unsigned long closedCount=0;
+    unsigned long closedCount=0,closedCount2=0;
     unsigned long min=4294967295;
+    int closed,check=1;
+    int closedNodes[nodeNumber];
+    if(*(graph)==0 && *(graph+1)==0 && *(graph+2)==0 && *(graph+3)==0)return 0;
     //unsigned long *costs = (unsigned long *)malloc(sizeof (unsigned long)*nodeNumber);
     //unsigned long *nodeCache = (unsigned long *)malloc(sizeof (unsigned long)*nodeNumber);
 
@@ -259,31 +266,42 @@ unsigned long dijkstra(unsigned long *graph,int nodeNumber,unsigned long costs[]
                     if(*(graph+row*nodeNumber+I)!=0 && *(costs+I)> (sum + *(graph+row*nodeNumber+I))){
                         *(costs+I)=sum+*(graph+row*nodeNumber+I);
                     }
-                    else if(*(graph+row*nodeNumber+I)==0) *(costs+I)=sum+*(graph+row*nodeNumber+I);
+                    else if(*(graph+row*nodeNumber+I)!=0 && *(costs+I)==0) *(costs+I)=sum+*(graph+row*nodeNumber+I);
                 }
             }
             else{
                 //iterates over row and chooses the closest node while updating paths
-                if(min>*(graph+row*nodeNumber+I)){
-                    min=*(graph+row*nodeNumber+I);
-                    *(nodeCache+closedCount) = I;
-                    //update when finds closer
+                if(min>*(graph+row*nodeNumber+I)&&*(graph+row*nodeNumber+I)!=0){
+                    for(closed=0;closed<closedCount;closed++){
+                        if(closedNodes[closed]==I) check=0;
+                    }
+                    if(check==1){
+                        min=*(graph+row*nodeNumber+I);
+                        *(nodeCache+closedCount) = I;
+                        //update when finds closer
+                    }
                 }
+                check=1;
                 if(closedCount==0)*(costs+I)=*(graph+I);//Initialize costs
                 else{ //Update costs if shorter paths are found (0 counts as infinite, so always update when 0)
                     if(*(graph+row*nodeNumber+I)!=0 && *(costs+I)> (sum + *(graph+row*nodeNumber+I))){
                         *(costs+I)=sum+*(graph+row*nodeNumber+I);
                     }
-                    else if(*(graph+row*nodeNumber+I)==0) *(costs+I)=sum+*(graph+row*nodeNumber+I);
+                    else if(*(graph+row*nodeNumber+I)!=0 && *(costs+I)==0) *(costs+I)=sum+*(graph+row*nodeNumber+I);
                 }
             }
         }
             //min=100000;
             //save node and update row when current row is finished
+            closedNodes[closedCount]=row;
             row = *(nodeCache+closedCount);
             closedCount++;
             if(closedCount<nodeNumber) sum+=min;
-            min=4294967295;
+            if(min==42949672950){
+                row=closedNodes[closedCount2-1];
+                if(closedCount==0)break;
+            }
+            min=42949672950;
     }
     //printf("bro %lu",min);
     unsigned long result;
@@ -299,7 +317,7 @@ unsigned long dijkstra(unsigned long *graph,int nodeNumber,unsigned long costs[]
 
 void buildMaxheapify(graphy A[],int length){
     int I;
-    for(I=length/2;I>0;I--){
+    for(I=length/2;I>=0;I--){
         maxheapify(A,I,length);
     }
 }
@@ -312,51 +330,158 @@ void maxheapify(graphy A[],int position,int length){
     if(left>length-1)left=length-1;
     right=(2*I)+2;
     if(right>length-1)right=length-1;
-    if((left<=length) && (A[left].value > A[position].value)){
+    if((left<=length-1) && (A[left].value > A[position].value)){
         posmax=left;
     }
-    else posmax=right;
-    if((right<=length) && (A[right].value > A[posmax].value)){
+    else posmax=position;
+    if((right<=length-1) && (A[right].value > A[posmax].value)){
         posmax=right;
     }
     if(posmax!=position){
-        dummy.value=A[position].value;
-        dummy.id=A[position].id;
-        A[position].value = A[posmax].value;
-        A[position].id = A[posmax].id;
-        A[posmax].value = dummy.value;
-        A[posmax].id = dummy.id;
+        //dummy.value=A[position].value;
+        //dummy.id=A[position].id;
+        dummy=A[position];
+        //A[position].value = A[posmax].value;
+        //A[position].id = A[posmax].id;
+        A[position]=A[posmax];
+        //A[posmax].value = dummy.value;
+        //A[posmax].id = dummy.id;
+        A[posmax]=dummy;
         maxheapify(A,posmax,length);
     }
 }
 
 void deleteMax(graphy A[],int length){
-    int I,max;
     if(length<1);
-    max=A[0].value;
-    A[0].value=A[length].value;
-    A[0].id=A[length].id;
-    A[length].value=A[length-1].value;
-    A[length].id=A[length-1].id;
+    //A[0].value=A[length].value;
+    //A[0].id=A[length].id;
+    A[0]=A[length-1];
+    //A[length].value=A[length-1].value;
+    //A[length].id=A[length-1].id;
+    length=length-1;
     maxheapify(A,0,length);
 }
 
 void insert(graphy A[],int length,unsigned long value,unsigned long id){
-    int I,max;
+    int I;
     graphy dummy;
-    A[length].value=A[length+1].value;
-    A[length].id=A[length+1].id;
-    A[length].value = value;
-    A[length].id = id;
+    //A[length].value=A[length+1].value;
+    //A[length].id=A[length+1].id;
+    length=length+1;
+    A[length-1].value = value;
+    A[length-1].id = id;
     I = length;
     while(I>0 && (A[(I-1)/2].value < A[I].value)){
-        dummy.value=A[(I-1)/2].value;
-        dummy.id=A[(I-1)/2].id;
-        A[(I-1)/2].value = A[I].value;
-        A[(I-1)/2].id = A[I].id;
-        A[I].value = dummy.value;
-        A[I].id = dummy.id;
+        //dummy.value=A[(I-1)/2].value;
+        //dummy.id=A[(I-1)/2].id;
+        dummy=A[(I-1)/2];
+        //A[(I-1)/2].value = A[I].value;
+        //A[(I-1)/2].id = A[I].id;
+        A[(I-1)/2]=A[I];
+        //A[I].value = dummy.value;
+        //A[I].id = dummy.id;
+        A[I]=dummy;
         I = (I-1)/2;
     }
 }
 
+unsigned long dijkstra2(unsigned long *graph,int nodeNumber,unsigned long costs[],unsigned long nodeCache[]){
+    unsigned long dist[nodeNumber],prev[nodeNumber],nodes[nodeNumber];
+    int I,II,III,IV;
+    int u=4294967295;
+    int closedNum=0;
+    int alt=0;
+    int min=4294967295;
+    for(I=0;I<nodeNumber;I++){
+        dist[I]=*(graph+I);
+    }
+    for(II=0;II<nodeNumber;II++){
+        prev[II]=-1;
+    }
+    for(III=0;III<nodeNumber;III++){
+        nodes[III]=0;
+    }
+    //dist[0]=0;
+    while(nodes[0]==0){
+        for(I=0;I<nodeNumber;I++){
+            if(min > dist[I] && dist[I]!=0){
+                min = dist[I];
+                u=I;
+            }
+        }
+        nodes[u]=1;
+        closedNum++;
+        if(u==4294967295)break;
+        for(IV=0;IV<nodeNumber;IV++){
+            if(*(graph+(u*nodeNumber)+IV)!=0){
+                alt=dist[u]+*(graph+(u*nodeNumber)+IV);
+                if(alt<dist[IV]){
+                    dist[IV]=alt;
+                    prev[IV] = u;
+                    //riordina
+                }
+            }
+        }
+        int sum,V;
+        for(V=0;V<nodeNumber;V++){
+            sum+=dist[V];
+        }
+        return sum;
+    }
+}
+
+
+    int minDistance(int dist[], int nodes[],int nodeNumber){
+        // Initialize min value
+        int min = 4294967295, min_index;
+
+        for (int v = 0; v < nodeNumber; v++)
+            if (nodes[v] == 0 && dist[v] <= min)
+                min = dist[v], min_index = v;
+
+            return min_index;
+    }
+
+
+    void dijkstra3(unsigned long *graph,int nodeNumber){
+        int i;
+        int dist[nodeNumber]; // The output array.  dist[i] will hold the shortest
+        // distance from src to i
+
+        int nodes[nodeNumber]; // sptSet[i] will be true if vertex i is included in shortest
+        // path tree or shortest distance from src to i is finalized
+
+        // Initialize all distances as INFINITE and stpSet[] as false
+        for (int i = 0; i < nodeNumber; i++)
+            dist[i] = 4294967295, nodes[i] = 0;
+
+        // Distance of source vertex from itself is always 0
+        dist[0] = *(graph);
+
+        // Find shortest path for all vertices
+        int count;
+        for (int count = 0; count < nodeNumber - 1; count++) {
+            // Pick the minimum distance vertex from the set of vertices not
+            // yet processed. u is always equal to src in the first iteration.
+            int u = minDistance(dist, nodes,nodeNumber);
+
+            // Mark the picked vertex as processed
+            nodes[u] = 1;
+
+            // Update dist value of the adjacent vertices of the picked vertex.
+            int v;
+            for (int v = 0; v < nodeNumber; v++)
+
+                // Update dist[v] only if is not in sptSet, there is an edge from
+                // u to v, and total weight of path from src to  v through u is
+                // smaller than current value of dist[v]
+                if (!nodes[v] && *(graph+(nodeNumber*v)+u) && dist[u] != 4294967295
+                && dist[u] + *(graph+(nodeNumber*v)+u) < dist[v])
+                    dist[v] = dist[u] + *(graph+(nodeNumber*v)+u);
+        }
+    }
+
+
+
+
+}
