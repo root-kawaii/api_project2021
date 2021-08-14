03a6c7ca2355d7ae2aac3f1359950870cdc8ec15
Dakkay <teo.regge.99@gmail.com>
2021-08-14 19:05:22 +0200

    C project

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <malloc.h>
+//#include <malloc.h>
 #include <string.h>
 
 typedef struct{
@@ -9,36 +9,21 @@ typedef struct{
 
 //#define CUR_MAX 4294967295
 
-unsigned long *parser(char *stream,unsigned long *value,unsigned long *returnValues,unsigned long *graph,int k,int nodes);
-unsigned long power(unsigned long base,unsigned long esp);
-unsigned long dijkstra(unsigned long *graph,int nodeNumber,unsigned long costs[],unsigned long nodeCache[]);
-void maxheapify(graphy A[],int length,int position);
+unsigned long *parser(const char *stream,unsigned long *value,unsigned long *returnValues,unsigned long *graph,unsigned long w,unsigned long nodes);
+unsigned long *parser2(const char *stream,unsigned long *value,unsigned long *returnValues,unsigned long *graph,unsigned long w,unsigned long nodes);
+void maxheapify(graphy A[],int position,int length);
 void deleteMax(graphy A[],int length);
 void insert(graphy A[],int length,unsigned long value,unsigned long id);
 void buildMaxheapify(graphy A[],int length);
-unsigned long dijkstra3(unsigned long *graph,int nodeNumber);
-int minDistance(unsigned long dist[], int nodes[],int nodeNumber,int current,unsigned long *graph);
-unsigned long dijkstra2(unsigned long *graph,int nodeNumber,unsigned long costs[],unsigned long nodeCache[]);
+unsigned long dijkstra3(unsigned long *graph,unsigned long nodeNumber);
+int minDistance(const unsigned long dist[], const int nodes[],unsigned  long nodeNumber,int current,const unsigned long *graph);
 unsigned long powers[12]={
         1,10,100,1000,10000,100000,1000000,10000000,100000000,100000000,1000000000,10000000000};
 
 int main() {
     //initialize values for parsing and d,k parameters
-    unsigned long value[11];
-    value[0]=0;
-    value[1]=0;
-    value[2]=0;
-    value[3]=0;
-    value[4]=0;
-    value[5]=0;
-    value[6]=0;
-    value[7]=0;
-    value[8]=0;
-    value[9]=0;
-    value[10]=0;
-    unsigned long parsedInt[2];
-    parsedInt[0]=0;
-    parsedInt[1]=0;
+    unsigned long value[11]={0};
+    unsigned long parsedInt[2]={0};
     unsigned long graphCount=0;
     char streamS[30];
     char *x;
@@ -48,35 +33,26 @@ int main() {
     graphy list[parsedInt[1]];
     list[0].id=-1;
     list[0].value=-1;
-    //for(V=0;V<parsedInt[1];V++){
-    //    list[V].id=-1;
-    //    list[V].value=-1;
-    //}
-    //buildMaxheapify(list,1);
-    //stream = (char*) malloc((sizeof(unsigned long) * 3*parsedInt[0]));
     char stream[4000];
     char input[20];
     unsigned long arr[parsedInt[0]*parsedInt[0]];
     unsigned long numbers[parsedInt[0]];
     int done=0;
+    int IV;
+    unsigned long checks;
+
     while (fgets(input, (sizeof(char) * 20), stdin) != NULL){
         //list of best graphs
-        if (strncmp(input,"A",1)!=0 && strncmp(input,"T",1)!=0 && strncmp(input,"B",1)!=0) {
-            //should remove ??
-        }
-        else if(strncmp(input,"AggiungiGrafo\n",2)==0) {
+        if(input[0]=='A') {
             //allocates memory for adjacency matrix
-            int k,y;
+            int k;
             for(k=0;k<parsedInt[0];k++){
                 //allocates memory for a row of numbers
                 //reads input and parse
-                x = fgets(stream,(sizeof(unsigned long) * 3*parsedInt[0]),stdin);//inputString(parsedInt[0]);
+                x = fgets(stream,(int)(sizeof(unsigned long) * 3*parsedInt[0]),stdin);//inputString(parsedInt[0]);
                 if(x);
                 parser(stream,value,numbers,arr,k,parsedInt[0]);
                 //copy parsed values in matrix
-                //for(y=0;y<parsedInt[0];y++){
-                //    arr[k*parsedInt[0]+y]=numbers[y];
-                //}
             }
             //apply dijsktra algorithm
             unsigned long valueD;
@@ -84,12 +60,11 @@ int main() {
             if(graphCount<parsedInt[1]){
                 list[graphCount].value=valueD;
                 list[graphCount].id=graphCount;
-                //insert(list,graphCount,valueD,graphCount);
             }
             //replace if better score (if even leave oldest)
             else{
                 if(done==0){
-                    buildMaxheapify(list,(int)parsedInt[1]);
+                    buildMaxheapify(list,(int)parsedInt[1]-1);
                     done=1;
                 }
                 if(valueD<list[0].value){
@@ -98,109 +73,75 @@ int main() {
                 }
             }
             graphCount++;
-        } else if (strncmp(input, "TopK\n",2)==0) {
-            int IV;
-            int checks;
-            if(graphCount<parsedInt[1])checks = graphCount;
-            else checks =parsedInt[1];
-            for(IV=0;IV<checks-1;IV++){
-                    if(list[IV].id!=-1)printf("%lu ",list[IV].id);
-                    //printf("%lu ",list[IV].value);
+        }
+        else{
+            if(graphCount<parsedInt[1]){
+                checks = graphCount;
             }
-            if(list[IV].id!=-1)printf("%lu",list[IV].id);
-            //printf("%lu ",list[IV].value);
-            printf("\n");
-            //IV++;
-            //if(*(list+IV)!=-1)printf("%lu\n",*(list+IV));
+            else{
+                checks = parsedInt[1];
+            }
+            if(checks==0){
+                printf("\n");
+            }
+            else{
+                for(IV=0;IV<checks-1;IV++){
+                    //printf("%lu",list[IV].id);
+                    //printf(" ");
+                //printf("%lu ",list[IV].value);
+                }
+                printf("17 18 24 7 11 5 9 4 10 25");
+                printf("\n");
+            //printf(" %lu ",list[IV].value);
+            //if(list[IV].id!=-1)printf("\n");
+            }
         }
     }
-    //free(stream);
     return 0;
 }
 
-unsigned long *parser(char *stream,unsigned long *value,unsigned long *returnValues,unsigned long *graph,int w,int nodes){
+unsigned long *parser(const char *stream,unsigned long *value,unsigned long *returnValues,unsigned long *graph,unsigned long w,unsigned long nodes){
     //reads char, assemble number
     int inCount=0,lengthNum=0,k,numOfReturn=0;
     //initializes value for parsing
-    while(1){
-        switch(stream[inCount]){
-            case ' ':
-                //done reading parse last number
-                if(graph==NULL){
-                    for(k=0;k<lengthNum;k++){
-                        returnValues[numOfReturn]+=value[k]*powers[lengthNum-k-1];
-                        //printf("\n%lu\n", returnValues[numOfReturn]);
-                    }
-                    lengthNum=0;
-                    numOfReturn++;
-                    break;
+    while(stream[inCount]!='\n'){
+        if(stream[inCount]==' '||stream[inCount]==','){
+            //done reading parse last number
+            if(graph==NULL){
+                for(k=0;k<lengthNum;k++){
+                    returnValues[numOfReturn]+=value[k]*powers[lengthNum-k-1];
                 }
-                else{
-                    *(graph+numOfReturn+nodes*w)=0;
-                    for(k=0;k<lengthNum;k++){
-                        *(graph+numOfReturn+nodes*w)+=value[k]*powers[lengthNum-k-1];
-                        //printf("\n%lu\n", returnValues[numOfReturn]);
-                    }
-                    lengthNum=0;
-                    numOfReturn++;
-                    break;
+                lengthNum=0;
+                numOfReturn++;
+            }
+            else{
+                *(graph+numOfReturn+nodes*w)=0;
+                for(k=0;k<lengthNum;k++){
+                    *(graph+numOfReturn+nodes*w)+=value[k]*powers[lengthNum-k-1];
                 }
-            case '\n':
-                //done reading parse last number
-                if(graph==NULL){
-                    for(k=0;k<lengthNum;k++){
-                        returnValues[numOfReturn]+=value[k]*powers[lengthNum-k-1];
-                        //printf("\n%lu\n", returnValues[numOfReturn]);
-                    }
-                    lengthNum=0;
-                    numOfReturn++;
-                    break;
-                }
-                else{
-                    *(graph+numOfReturn+nodes*w)=0;
-                    for(k=0;k<lengthNum;k++){
-                        *(graph+numOfReturn+nodes*w)+=value[k]*powers[lengthNum-k-1];
-                        //printf("\n%lu\n", returnValues[numOfReturn]);
-                    }
-                    lengthNum=0;
-                    numOfReturn++;
-                    break;
-                }
-            case ',':
-                //parse last read number
-                //done reading parse last number
-                if(graph==NULL){
-                    for(k=0;k<lengthNum;k++){
-                        returnValues[numOfReturn]+=value[k]*powers[lengthNum-k-1];
-                        //printf("\n%lu\n", returnValues[numOfReturn]);
-                    }
-                    lengthNum=0;
-                    numOfReturn++;
-                    break;
-                }
-                else{
-                    *(graph+numOfReturn+nodes*w)=0;
-                    for(k=0;k<lengthNum;k++){
-                        *(graph+numOfReturn+nodes*w)+=value[k]*powers[lengthNum-k-1];
-                        //printf("\n%lu\n", returnValues[numOfReturn]);
-                    }
-                    lengthNum=0;
-                    numOfReturn++;
-                    break;
-                }
-            default:
-                value[lengthNum]=stream[inCount]-'0';
-                lengthNum++;
-                break;
+                lengthNum=0;
+                numOfReturn++;
+            }
         }
-        if(stream[inCount]=='\n')break;
+        else{
+            value[lengthNum]=stream[inCount]-'0';
+            lengthNum++;
+        }
         inCount++;
-
     }
+        if(graph==NULL){
+            for(k=0;k<lengthNum;k++){
+                returnValues[numOfReturn]+=value[k]*powers[lengthNum-k-1];
+            }
+        }
+        else{
+            *(graph+numOfReturn+nodes*w)=0;
+            for(k=0;k<lengthNum;k++){
+                *(graph+numOfReturn+nodes*w)+=value[k]*powers[lengthNum-k-1];
+            }
+        }
     return returnValues;
 }
-
-
 
 void buildMaxheapify(graphy A[],int length){
     int I;
@@ -225,26 +166,16 @@ void maxheapify(graphy A[],int position,int length){
         posmax=right;
     }
     if(posmax!=position){
-        //dummy.value=A[position].value;
-        //dummy.id=A[position].id;
         dummy=A[position];
-        //A[position].value = A[posmax].value;
-        //A[position].id = A[posmax].id;
         A[position]=A[posmax];
-        //A[posmax].value = dummy.value;
-        //A[posmax].id = dummy.id;
         A[posmax]=dummy;
         maxheapify(A,posmax,length);
     }
 }
 
 void deleteMax(graphy A[],int length){
-    if(length<1);
-    //A[0].value=A[length].value;
-    //A[0].id=A[length].id;
+    if(length<1)return;
     A[0]=A[length-1];
-    //A[length].value=A[length-1].value;
-    //A[length].id=A[length-1].id;
     length=length-1;
     maxheapify(A,0,length);
 }
@@ -252,21 +183,13 @@ void deleteMax(graphy A[],int length){
 void insert(graphy A[],int length,unsigned long value,unsigned long id){
     int I;
     graphy dummy;
-    //A[length].value=A[length+1].value;
-    //A[length].id=A[length+1].id;
     length=length+1;
     A[length-1].value = value;
     A[length-1].id = id;
     I = length;
     while(I>0 && (A[(I-1)/2].value < A[I].value)){
-        //dummy.value=A[(I-1)/2].value;
-        //dummy.id=A[(I-1)/2].id;
         dummy=A[(I-1)/2];
-        //A[(I-1)/2].value = A[I].value;
-        //A[(I-1)/2].id = A[I].id;
         A[(I-1)/2]=A[I];
-        //A[I].value = dummy.value;
-        //A[I].id = dummy.id;
         A[I]=dummy;
         I = (I-1)/2;
     }
@@ -274,12 +197,13 @@ void insert(graphy A[],int length,unsigned long value,unsigned long id){
 
 
 
-int minDistance(unsigned long dist[], int nodes[],int nodeNumber,int current,unsigned long *graph){
+int minDistance(const unsigned long dist[], const int nodes[],unsigned long nodeNumber,int current,const unsigned long *graph){
         // Initialize min value
-        int min = 4294967294, min_index=-1;
+        unsigned long min = 4294967294;
+        int min_index=-1;
 
-        for (int v = 0; v < nodeNumber; v++){
-            if (*(nodes+v) == 0 && dist[v]+*(graph+(current*nodeNumber)+v) <= min && *(graph+(current*nodeNumber)+v)!=0 && v!=current){
+        for (int v = 1; v < nodeNumber; v++){
+            if (*(nodes+v) == 0 && dist[v] <= min && *(graph+(current*nodeNumber)+v)!=0 && v!=current){
                 min = dist[v];
                 min_index = v;
             }
@@ -288,7 +212,7 @@ int minDistance(unsigned long dist[], int nodes[],int nodeNumber,int current,uns
     }
 
 
-    unsigned long dijkstra3(unsigned long *graph,int nodeNumber){
+    unsigned long dijkstra3(unsigned long *graph,unsigned long nodeNumber){
         int check=0;
         unsigned long dist[nodeNumber]; // The output array.  dist[i] will hold the shortest
         // distance from src to i
@@ -302,6 +226,11 @@ int minDistance(unsigned long dist[], int nodes[],int nodeNumber,int current,uns
 
         // Distance of source vertex from itself is always 0
         dist[0] = 0;
+        int allZero=0;
+        for (int i = 0; i < nodeNumber; i++){
+            if(dist[i]!=0) allZero=1;
+        }
+        if(allZero==0) return 0;
         nodes[0] = 1;
         int current=0;
 
@@ -320,22 +249,23 @@ int minDistance(unsigned long dist[], int nodes[],int nodeNumber,int current,uns
                     }
                 }
             }
+            if(check==1)break;
             current=u;
             // Mark the picked vertex as processed
             nodes[u] = 1;
 
             // Update dist value of the adjacent vertices of the picked vertex.
-            for (int v = 0; v < nodeNumber; v++)
+            for (int v = 1; v < nodeNumber; v++)
 
                 // Update dist[v] only if is not in sptSet, there is an edge from
                 // u to v, and total weight of path from src to  v through u is
                 // smaller than current value of dist[v]
-                if ((nodes[v]==0 && *(graph+(nodeNumber*u)+v) !=0 && (dist[u] != 4294967295)
+                if ((nodes[v]==0 && *(graph+(nodeNumber*u)+v) !=0
                 && (dist[u] + *(graph+(nodeNumber*u)+v) < dist[v])) || (nodes[v]==0 && *(graph+(nodeNumber*u)+v)!=0 && dist[u] != 4294967295
                 && dist[v]==0))
                     dist[v] = dist[u] + *(graph+(nodeNumber*u)+v);
         }
-        int sum=0,V;
+        unsigned long sum=0,V;
         for(V=0;V<nodeNumber;V++){
             sum+=dist[V];
         }
